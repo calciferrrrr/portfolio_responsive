ef34ee6e9db8de16fbbb36206803fa0e74f14381
calciferrrrr <111638009+calciferrrrr@users.noreply.github.com>
2024-05-12 22:41:43 +0200

    Update bottom.js

--- bottom.js
+++ bottom.js
@@ -5,7 +5,7 @@ if (window.matchMedia("(max-width: 995px)").matches) {
     function scrollToBottom() {
         var bottomElement = document.querySelector('.bottom');
         var bottomPosition = bottomElement.getBoundingClientRect().top;
-        var offset = 130; // Puoi regolare questo valore per personalizzare l'offset
+        var offset = 150; // Puoi regolare questo valore per personalizzare l'offset
     
         // Calcola la posizione per fermare lo scorrimento prima di raggiungere l'elemento "bottom"
         var stopPosition = bottomPosition - offset;
